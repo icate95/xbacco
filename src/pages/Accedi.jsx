d0642914c0a28c88effba0066606a8c9fe7ad97d
fabio.pergamiferoldi@gmail.com <fabio.pergamiferoldi@gmail.com>
2019-04-01 16:54:25 +0200

    form accedi e registrati

--- src/pages/Accedi.jsx
+++ src/pages/Accedi.jsx
@@ -10,15 +10,15 @@ class Accedi extends React.Component {
       <>
       <section className='bgc-img-accedi'>
           <h1>--</h1>
+      </section>
       <div className="modal">
-            <h2>Login</h2>
-            <label htmlFor="nome">Nome</label>
-            <input type="text" name='name' placeholder='name'/>
-            <label htmlFor="password">Password</label>
-            <input type="password" name='password' placeholder='password'/>
-            <button className="btn-light">Login</button>
-        </div>
-     </section>
+          <h2>Login</h2>
+          <label htmlFor="nome">Nome</label>
+          <input type="text" name='name' placeholder='name'/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password' placeholder='password'/>
+          <button className="btn-light">Login</button>
+      </div>
       </>
     );
   }
--- src/pages/Registrati.jsx
+++ src/pages/Registrati.jsx
@@ -8,21 +8,21 @@ class Registrati extends React.Component {
   render() {
     return (
       <>
-     <section className='bgc-img-registrati'>
-     <h1>--</h1>
-      <div className="modal">
+        <section className='bgc-img-registrati'>
+            <h1>--</h1>
+        </section>
+        <div className="modal">
             <h2>Registrati</h2>
             <label htmlFor="nome">Nome</label>
-            <input type="text" name='name' placeholder='name'/>
+            <input type="text" name='name' placeholder='nome'/>
             <label htmlFor="data">Data di nascita <br/><small>devi essere maggiorenne per iscriverti al nostro servizio</small></label>
-            <input type="data" name='data'/>
+            <input type="date" name='data'/>
             <label htmlFor="indirizzo">Indirizzo</label>
             <input type="text" name='indirizzo' placeholder='Indirizzo'/>
             <label htmlFor="password">Password</label>
             <input type="password" name='password' placeholder='password'/>
             <button className="btn-light">Registrati</button>
         </div>
-     </section>
       </>
     );
   }
