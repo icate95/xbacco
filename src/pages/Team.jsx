ff4df4c9be90920b66f1fd9f9c0927a7288c3804
fabio.pergamiferoldi@gmail.com <fabio.pergamiferoldi@gmail.com>
2019-04-02 17:56:42 +0200

    foto team

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -3,25 +3,25 @@ import Footer from "./components/Footer";
 import SVG from "./SVG";
 import logoColore from "../img/logo1.png";
 import { Link } from "react-router-dom";
-import foto_CV from "../img/foto-CV.jpg";
+import foto_CV from "../img/foto_CV.jpg";
 
 class Team extends React.Component {
   render() {
     return (
       <>
-        <h2>Il nostro Team</h2>
-        <div className="photo-container">
-          <img src={foto_CV} alt="Fabio Pergami"/>
-          <img src="" alt=""/>
-          <img src="" alt=""/>
-          <img src="" alt=""/>
-        </div>
-        <div className="photo-container">
-          <img src={foto_CV} alt="Fabio Pergami"/>
-          <img src="" alt=""/>
-          <img src="" alt=""/>
-          <img src="" alt=""/>
-        </div>
+        <section className="padding">
+          <h2>Il nostro Team</h2>
+          <div className="padding photo-container">
+            <img src={foto_CV} alt="Fabio Pergami"/>
+            <img src={foto_CV} alt="Fabio Pergami"/>
+            <img src={foto_CV} alt="Fabio Pergami"/>
+          </div>
+          <div className="padding">
+            <img src={foto_CV} alt="Fabio Pergami"/>
+            <img src={foto_CV} alt="Fabio Pergami"/>
+            <img src={foto_CV} alt="Fabio Pergami"/>
+          </div>
+        </section>
       </>
     );
   }
