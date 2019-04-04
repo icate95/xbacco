539af2501c8e32e44df2e4a41648eab9fd7b0e3b
fabio.pergamiferoldi@gmail.com <fabio.pergamiferoldi@gmail.com>
2019-04-04 13:29:27 +0200

    team foto

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,6 +4,11 @@ import SVG from "./SVG";
 import logoColore from "../img/logo1.png";
 import { Link } from "react-router-dom";
 import foto_CV from "../img/foto_CV.jpg";
+import allegra from "../img/allegra.jpg";
+import alessia from "../img/alessia.JPG";
+import sofia from "../img/sofia.jpg";
+import cate from "../img/cate.jpg";
+import silvia_foto from "../img/silvia_foto.jpg";
 
 class Team extends React.Component {
   render() {
@@ -11,14 +16,14 @@ class Team extends React.Component {
       <>
         <section className="padding">
           <h2>Il nostro Team</h2>
-          <div className="padding photo-container">
-            <img src={foto_CV} alt="Fabio Pergami"/>
-            <img src={foto_CV} alt="Fabio Pergami"/>
-            <img src={foto_CV} alt="Fabio Pergami"/>
+          <div className="padding">
+            <img src={allegra} alt="Allegra Graziani"/>
+            <img src={alessia} alt="Alessia Porro"/>
+            <img src={sofia} alt="Sofia Bani"/>
           </div>
           <div className="padding">
-            <img src={foto_CV} alt="Fabio Pergami"/>
-            <img src={foto_CV} alt="Fabio Pergami"/>
+            <img src={cate} alt="Caterina Ianeselli"/>
+            <img src={silvia_foto} alt="Silvia Monzani"/>
             <img src={foto_CV} alt="Fabio Pergami"/>
           </div>
         </section>
